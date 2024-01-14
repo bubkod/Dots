ddb6b543ea8fa45bde1b4fd50b7c02629d645a76
bubkod <tankistking@gmail.com>
2024-01-14 17:04:29 +0200

    renamed:    plzexecute.sh -> installeverything.sh

--- installeverything.sh
+++ installeverything.sh
@@ -1,2 +1,6 @@
+# Unrecommended if you want to keep some of your configurations or something idk :shrug:.
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cp -r ./.config ~/.config
+cp -r ./.fonts ~/.fonts
+cp -r ./.bashrc ~/.bashrc
