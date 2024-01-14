c67c3ccd4f72d0fa4da9a3b0cf576e41224f52f3
bubkod <tankistking@gmail.com>
2024-01-14 17:10:47 +0200

    Added notes to the fonts section, also made the script to auto execute fc-cache -fvr

--- installeverything.sh
+++ installeverything.sh
@@ -2,5 +2,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cp -r ./.config ~/.config
-cp -r ./.fonts ~/.fonts
 cp -r ./.bashrc ~/.bashrc
+# These two will install some fonts to make FireFox and other apps work betterer (Most fonts by Microsoft atm).
+cp -r ./.fonts ~/.fonts
+fc-cache -fvr
