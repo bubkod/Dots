3eabde9576259f912f2bff199f327d5e99b0f6b6
bubkod <59019014+bubkod@users.noreply.github.com>
2024-02-14 17:16:06 +0000

    Update installeverything.sh
    I fix some things after actually trying.

--- installeverything.sh
+++ installeverything.sh
@@ -1,8 +1,13 @@
-# Unrecommended if you want to keep some of your configurations or something idk :shrug:.
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-cp -r ./.config ~/.config
+# BRATAN THIS IS PERSONAL!!
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/nvim
+
+cp -r ./.config/nvim/init.vim ~/.config/nvim/init.vim
+cp -r ./.config/alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
 cp -r ./.bashrc ~/.bashrc
 # These two will install some fonts to make FireFox and other apps work betterer (Most fonts by Microsoft atm).
 cp -r ./.fonts ~/.fonts
+
 fc-cache -fvr
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
