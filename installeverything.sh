77c13d483b60e09405a95cbce246bfa05552d760
bubkod <59019014+bubkod@users.noreply.github.com>
2024-02-19 12:24:05 +0000

    Update installeverything.sh
    oops..

--- installeverything.sh
+++ installeverything.sh
@@ -1,1 +1,11 @@
+# BRATAN THIS IS PERSONAL!!
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/nvim
 
+cp -r ./.config ~/
+# These two will install some fonts to make FireFox and other apps work betterer (Most fonts by Microsoft atm).
+cp -r ./.fonts ~/.fonts
+
+fc-cache -fvr
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
